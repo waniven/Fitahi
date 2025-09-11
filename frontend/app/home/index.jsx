68e8c4e2f1a01496b96088a2c42c8b6ae0e6eefc
Natalie25-4 <162855358+Natalie25-4@users.noreply.github.com>
2025-09-12 00:28:04 +1200

    reusable nac bar, update home screen to use component

--- frontend/app/home/index.jsx
+++ frontend/app/home/index.jsx
@@ -10,6 +10,7 @@ import FitahiLogo from "../../constants/FitahiLogo";
 import { Calendar } from "react-native-calendars";
 import Toast from "react-native-toast-message";
 import globalStyles from "../../styles/globalStyles";
+import BottomNav from "@/components/navbar/bottomnav";
 
 const { width } = Dimensions.get("window");
 
@@ -217,27 +218,7 @@ export default function Home() {
       </ScrollView>
 
       {/* bottom navigation */}
-      <View style={[styles.bottomNav, { backgroundColor: "#fff" }]}>
-        <TouchableOpacity style={styles.navItem} onPress={() => router.push("/home/index")}>
-          <Ionicons name="home-outline" size={26} color={theme.tint} />
-          <Text style={[globalStyles.navText, { color: theme.tint }]}>Home</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.navItem} onPress={() => router.push("/main/analytics")}>
-          <Ionicons name="stats-chart-outline" size={26} color={theme.tint} />
-          <Text style={[globalStyles.navText, { color: theme.tint }]}>Analytics</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.navItem} onPress={() => router.push("/main/supplements")}>
-          <Ionicons name="medkit-outline" size={26} color={theme.tint} />
-          <Text style={[globalStyles.navText, { color: theme.tint }]}>Supplements</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.navItem} onPress={() => router.push("/profile/AccountSettings")}>
-          <Ionicons name="settings-outline" size={26} color={theme.tint} />
-          <Text style={[globalStyles.navText, { color: theme.tint }]}>Settings</Text>
-        </TouchableOpacity>
-      </View>
+      <BottomNav/>
 
       {/* floating ai button */}
       <FloatingAIButton />
@@ -355,22 +336,6 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.1,
     shadowRadius: 6,
     elevation: 3
-  },
-
-  // Bottom navigation
-  bottomNav: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    paddingVertical: 12,
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
-    position: "absolute",
-    bottom: 0,
-    width: "100%",
-    pointerEvents: "box-none",
-  },
-  navItem: {
-    alignItems: "center"
   },
 
   // Modal
