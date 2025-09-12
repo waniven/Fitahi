4cc933610a75154f0e77c5480814a64a47221d04
as178 <shadowangel753@gmail.com>
2025-09-12 22:05:18 +1200

    set up services and modified routes to be compatible with frontend after looking at the code for workout results

--- frontend/services/workoutService.js
+++ frontend/services/workoutService.js
@@ -1,31 +1,30 @@
 import api from "./api";
 
-// get all workouts for logged-in user
+// GET all workouts for logged-in user
 export async function getWorkouts() {
-    console.log("👉 Sending headers:", api.defaults.headers);
     const res = await api.get("/workouts");
     return res.data;
 };
 
-// get a single workout by id
+// GET a single workout by id
 export async function getWorkoutById(id) {
     const res = await api.get(`/workouts/${id}`);
     return res.data;
 };
 
-// create a new workout
+// POST a new workout
 export async function createWorkout(workout) {
     const res = await api.post("/workouts", workout);
     return res.data;
 };
 
-// update an existing workout
+// PATCH an existing workout
 export async function updateWorkout(id, workout) {
     const res = await api.patch(`/workouts/${id}`, workout);
     return res.data;
 };
 
-// delete a workout
+// DELETE a workout
 export async function deleteWorkout(id) {
     await api.delete(`/workouts/${id}`);
 };
